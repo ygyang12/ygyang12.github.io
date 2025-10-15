016ad9fad0bfba5001029cddec98bffe7baed115
ygyang12 <1436777478@qq.com>
2025-10-15 20:24:55 +0800

    1015

--- run_server.sh
+++ run_server.sh
@@ -1,1 +1,7 @@
-bundle exec jekyll liveserve
+#!/usr/bin/env bash
+
+# Use standard Jekyll server to avoid incompatibilities with hawkins/liveserve
+# on Ruby 3.4 + Jekyll 3.9 + WEBrick 1.9.
+
+# Ensure development env and force polling (important on /Volumes/*)
+JEKYLL_ENV=development bundle exec jekyll serve --force_polling
